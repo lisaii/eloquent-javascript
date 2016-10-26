9fe821976ffaa1540b86129414961837cb57879a
lisaii <staysick713@github.com>
2016-10-26 16:38:08 +0300

    added some changes in the task of Bs

--- chapter3/countB's.js
+++ chapter3/countB's.js
@@ -1,10 +1,10 @@
-function countBs(str) {
+function countBs(str, symbol) {
     var countOfB = 0;
     for (var i = 0; i < str.length; i++) {
-        if (str.charAt(i) === 'b') {
+        if (str.charAt(i) === symbol) {
             countOfB++
         }
     }
     return countOfB
 }
-console.log(countBs('rnfdnbdfdbdb'));
+console.log(countBs('rnfdnbdf#3##33#dbdb', '#'));
