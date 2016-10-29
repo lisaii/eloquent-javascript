aa732a103175902df4ca8d3e38faf3c766af4053
lisaii <staysick713@github.com>
2016-10-29 19:34:13 +0300

    added some arrays

---
+++ chapterSquirrel/arrays.js
@@ -0,0 +1,12 @@
+let mack = [];
+mack.push("Лисай");
+mack.push("учит", "жс");
+mack.push("алло", "хей");
+
+
+console.log(mack);
+//[ 'Лисай', 'учит', 'жс', 'алло', 'хей' ]
+console.log(mack.join(" "));
+//Лисай учит жс алло хей
+console.log(mack.pop());
+//хей
