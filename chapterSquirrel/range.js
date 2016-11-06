6c2befc9d0b24b659a8be6028228ada657b23a75
lisaii <staysick713@github.com>
2016-11-06 14:11:28 +0300

    added first web-task page

--- chapterSquirrel/range.js
+++ chapterSquirrel/range.js
@@ -1,8 +1,17 @@
 function range(first, last) {
-    let b = [];
+    const b = [];
     for (let a = first; a <= last; a++) {
         b.push(a)
     }
     return b
 }
-console.log(range(1, 10));
+
+function sum(array){
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum
+}
+console.log(range(1,10).reduce((a,b) => (a+b)))
+// console.log(sum(range(1, 10)));
