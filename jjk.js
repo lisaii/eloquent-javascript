6bbcd9bf13f1f6d594d4efe6dd4ca0d5b73915f6
lisaii <lisaishsb@gmail.com>
2016-12-05 23:32:25 +0300

    added pooooohui

--- jjk.js
+++ jjk.js
@@ -1,11 +1,10 @@
-i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-k = i[0];
-i[0] = i[i.length - 1];
-i[i.length - 1] = k;
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+k = [];
+ for  (let i = 0; i < arr.length; i++) {
 
+     k[i] = arr[arr.length - 1 - i];
 
-p = i[1];
-i[1] = i[i.length - 2];
-i[i.length - 2] = p;
+ }
 
-console.log(i);
+     console.log(k);
+
